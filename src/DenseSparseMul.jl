180ffa45ae2326f1744270b4436d7826956f029c
Romeo Valentin <romeov@stanford.edu>
2023-11-22 01:37:05 -0800

    Add threaded dense*sparse matmul
    From my benchmarks, the performance seems to be on par with IntelMKL :)

---
+++ src/DenseSparseMul.jl
@@ -0,0 +1,15 @@
+module DenseSparseMul
+
+import SparseArrays
+import SparseArrays: SparseMatrixCSC, mul!
+import Polyester: @batch
+
+function SparseArrays.mul!(C::AbstractMatrix, A::AbstractMatrix, B::SparseMatrixCSC, α::Number, β::Number)
+    @batch for j in axes(B, 2)
+        C[:, j] .*= β
+        C[:, j] .+= A * (α.*B[:, j])
+    end
+    return C
+end
+
+end # module DenseSparseMul
