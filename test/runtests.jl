78fdf5deb9ccaf0fd2cf4d19fda4a74220e87118
Romeo Valentin <romeov@stanford.edu>
2023-11-22 01:38:09 -0800

    Add simple test comparing to dense*dense mul.

---
+++ test/runtests.jl
@@ -0,0 +1,29 @@
+using DenseSparseMul, SparseArrays
+using Test
+import SparseArrays: _spmul!, mul!
+
+SparseArrays._spmul!(C::Matrix{Float64}, X::Matrix{Float64}, A::SparseMatrixCSC{Float64, Int64}, α::Number, β::Number) = @assert false
+
+@testset "Compare with equivalent dense mul" begin
+  @testset for trial in 1:10
+    lhs = rand(100, 120);
+    rhs = sprand(120, 140, 0.1);
+
+    baseline = lhs * Matrix(rhs);
+    @test lhs * rhs ≈ baseline
+
+    buf = similar(baseline)
+    buf .= lhs * rhs
+    @test buf ≈ baseline
+
+    buf .= 0.
+    buf .+= lhs * rhs
+    @test buf ≈ baseline
+
+    mul!(buf, lhs, rhs, 1, 0)
+    @test buf ≈ baseline
+
+    # make sure I've overwritten the regular spmul correctly...
+    @test_throws AssertionError _spmul!(buf, lhs, rhs, 1., 1.)
+  end
+end
