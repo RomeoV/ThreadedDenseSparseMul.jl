1c3703b650e25189112dbcac8bbfa4200efa0bc6
Romeo Valentin <romeov@stanford.edu>
2023-11-26 16:48:15 -0800

    Remove `_spmul!` as it's not backwards compat and increase problem size
    The profiling method only works for larger problems.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,36 @@
-using ThreadedDenseSparseMul, SparseArrays
+using ThreadedDenseSparseMul
 using Test
-import SparseArrays: _spmul!, mul!
+import SparseArrays: mul!, sprand
+import Profile
 
-SparseArrays._spmul!(C::Matrix{Float64}, X::Matrix{Float64}, A::SparseMatrixCSC{Float64, Int64}, α::Number, β::Number) = @assert false
+∈ₛ(a,b) = occursin(a,b)
+# We use a hack to find out if Polyester is actually used, namely
+# profiling the call and checking the used modules for Polyester.
+# This only works if the problem size is large enough (and then is still a statistical property).
+# I wasn't able to find a clear way, but probably there is one.
+function check_module_used(mod_string::String, prof_data::Vector)
+  cache = Dict{Symbol, String}();
+  sframes = Profile.getdict(prof_data) |> values |> Iterators.flatten
+  used_paths = [Profile.short_path(frame.file, cache) for frame in sframes]
+  any(path->mod_string ∈ₛ path, used_paths)
+end
 
 @testset "Compare with equivalent dense mul" begin
   @testset for trial in 1:10
-    lhs = rand(100, 120);
-    rhs = sprand(120, 140, 0.1);
+    lhs = rand(500, 1000);
+    rhs = sprand(1000, 10_000, 0.1);
 
     baseline = lhs * Matrix(rhs);
     @test lhs * rhs ≈ baseline
 
     buf = similar(baseline)
+
+    @testset "Check actually using Polyester" begin
+        # make sure I've overwritten the regular mul correctly and actually use Polyester...
+        Profile.clear(); Profile.@profile (buf .= lhs * rhs)
+        @test check_module_used("@Polyester", Profile.fetch())
+    end
+
     buf .= lhs * rhs
     @test buf ≈ baseline
 
@@ -26,8 +44,5 @@ SparseArrays._spmul!(C::Matrix{Float64}, X::Matrix{Float64}, A::SparseMatrixCSC{
 
     mul!(buf, lhs, rhs, 1, 0)
     @test buf ≈ baseline
-
-    # make sure I've overwritten the regular spmul correctly...
-    @test_throws AssertionError _spmul!(buf, lhs, rhs, 1., 1.)
   end
 end
