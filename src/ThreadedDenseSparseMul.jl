3ac85f99df20ac436c16e56c7d714de534f31382
Romeo Valentin <romeov@stanford.edu>
2023-11-29 13:21:05 -0800

    Write new API with threaded and non-threaded.

--- src/ThreadedDenseSparseMul.jl
+++ src/ThreadedDenseSparseMul.jl
@@ -1,15 +1,53 @@
 module ThreadedDenseSparseMul
 
 import SparseArrays
-import SparseArrays: SparseMatrixCSC, mul!
+import SparseArrays: SparseMatrixCSC, nonzeroinds
 import Polyester: @batch
 
-function SparseArrays.mul!(C::AbstractMatrix, A::AbstractMatrix, B::SparseMatrixCSC, α::Number, β::Number)
+"""
+    fastdensesparse!(C, A, B, α, β)
+
+BLAS like interface, computing `C .= β*C + α*A*B`, but accelerated with multi-threading using `Polyester.jl`.
+"""
+function fastdensesparse!(C::Matrix{T}, A::Matrix{T}, B::SparseMatrixCSC{T}, α::Number, β::Number) where T
+    for j in axes(B, 2)
+        C[:, j] .*= β
+        C[:, j] .+= A * (α.*B[:, j])
+    end
+    return C
+end
+function fastdensesparse_threaded!(C::Matrix{T}, A::Matrix{T}, B::SparseMatrixCSC{T}, α::Number, β::Number) where T
     @batch for j in axes(B, 2)
         C[:, j] .*= β
         C[:, j] .+= A * (α.*B[:, j])
     end
     return C
 end
+
+const VecOrView{T} = Union{Vector{T}, SubArray{T, 1, Matrix{T}}}
+function _fastdensesparse_outer!(C::Matrix{T}, A::VecOrView{T}, b::SparseVector{T}, α::Number, β::Number) where T
+    for (j, X_val) in zip(findnz(b)...)
+        C[:, j] .*= β
+        C[:, j] .+=  (α*X_val) .* A  # this compiles to something similar to axpy!, i.e. no allocations. Notice we need the dot also for the scalar mul.
+    end
+    return C
+end
+
+function fastdensesparse_outer!(C::Matrix{T}, a::VecOrView{T}, b::SparseVector{T}, α::Number, β::Number) where T
+    C[:, nonzeroinds(b)] .+=  a * nonzeros(b)'
+    return C
+end
+
+function fastdensesparse_outer_threaded!(C::Matrix{T}, A::VecOrView{T}, b::SparseVector{T}, α::Number, β::Number) where T
+    inds = nonzeroinds(b)
+    nzs = nonzeros(X)
+    @batch for i in inds
+        C[:, i] .*=  β
+        C[:, i] .+=  (α.*nzs[i]).*a
+    end
+    return C
+end
+
+
 
 end # module ThreadedDenseSparseMul
