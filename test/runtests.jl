264b8e9a8d167c45cce7bc96756b470789c98af6
Romeo Valentin <romeov@stanford.edu>
2024-01-06 14:09:05 -0800

    Fix tests by fixing `similar` call.

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,56 +6,69 @@ import Profile
 
 @testset "Compare with equivalent dense mul" begin
   @testset "fastdensesparsemul" begin
-    @testset for trial in 1:10
-      lhs = rand(500, 1000);
-      rhs = sprand(1000, 10_000, 0.1);
+    @testset for T in [Float64, Float32]
+      @testset for method! in [fastdensesparsemul!, fastdensesparsemul_threaded!]
+        @testset for trial in 1:10
+          lhs = rand(T, 50, 100);
+          rhs = sprand(T, 100, 1_000, 0.1);
 
-      baseline = lhs * Matrix(rhs);
+          baseline = lhs * rhs;
 
-      buf = similar(baseline)
+          # buf = 0*similar(baseline)  # <- BUG!! See https://discourse.julialang.org/t/occasionally-nans-when-using-similar/48224/12
+          # use instead
+          # buf = zeros(T, size(baseline))
+          # buf = similar(baseline); buf .= zero(T)
+          # buf = similar(baseline) .* false
+          buf = similar(baseline); buf .= zero(T)
 
-      fastdensesparsemul!(buf, lhs, rhs, 1, 0)
-      @test buf ≈ baseline
-      fastdensesparsemul!(buf, lhs, rhs, -1, 0)
-      fastdensesparsemul_threaded!(buf, lhs, rhs, 1, 0)
-      @test buf ≈ baseline
+          method!(buf, lhs, rhs, 1, 0)
+          @test buf ≈ baseline; @test !any(isnan, buf)
+          method!(buf, lhs, rhs, -1, 0)
+          method!(buf, lhs, rhs, 1, 0)
+          @test buf ≈ baseline; @test !any(isnan, buf)
 
 
-      # test @view interface and \beta \neq 0
-      inds = collect(3:5:100)
-      baseline[inds, :] .+= 2.5 * @view(lhs[inds, :]) * Matrix(rhs);
+          # test @view interface and \beta \neq 0
+          inds = collect(3:5:50)
+          baseline[inds, :] .+= 2.5 * @view(lhs[inds, :]) * rhs;
 
-      fastdensesparsemul!(@view(buf[inds, :]), @view(lhs[inds, :]), rhs, 2.5, 1)
-      @test buf ≈ baseline rtol=sqrt(eps(eltype(baseline)))
-      fastdensesparsemul!(@view(buf[inds, :]), @view(lhs[inds, :]), rhs, -2.5, 1)
-      fastdensesparsemul_threaded!(@view(buf[inds, :]), @view(lhs[inds, :]), rhs, 2.5, 1)
-      @test buf ≈ baseline rtol=sqrt(eps(eltype(baseline)))
+          method!(@view(buf[inds, :]), @view(lhs[inds, :]), rhs, 2.5, 1)
+          @test buf ≈ baseline rtol=sqrt(eps(eltype(baseline))); @test !any(isnan, buf)
+          method!(@view(buf[inds, :]), @view(lhs[inds, :]), rhs, -2.5, 1)
+          method!(@view(buf[inds, :]), @view(lhs[inds, :]), rhs, 2.5, 1)
+          @test buf ≈ baseline rtol=sqrt(eps(eltype(baseline))); @test !any(isnan, buf)
+        end
+      end
     end
   end
 
   @testset "fastdensesparsemul_outer" begin
-    @testset for trial in 1:10
-      lhs = rand(500, 1000);
-      rhs = sprand(1000, 10_000, 0.1);
-      k = rand(1:size(rhs, 1))
+    @testset for T in [Float64, Float32]
+      @testset for method! in [fastdensesparsemul_outer!, fastdensesparsemul_outer_threaded!]
+        @testset for trial in 1:10
+          lhs = rand(T, 50, 100);
+          rhs = sprand(T, 100, 1_000, 0.1);
+          k = rand(1:size(rhs, 1))
 
-      baseline = lhs[:, k:k] * Matrix(rhs)[k:k, :];
+          baseline = lhs[:, k:k] * rhs[k:k, :];
 
-      buf = similar(baseline)
+          buf = zeros(T, size(baseline))
 
-      fastdensesparsemul_outer!(buf, @view(lhs[:, k]), rhs[k, :], 1, 0)
-      @test buf ≈ baseline
-      fastdensesparsemul_outer!(buf, @view(lhs[:, k]), rhs[k, :], -1, 0)
-      fastdensesparsemul_outer_threaded!(buf, @view(lhs[:, k]), rhs[k, :], 1, 0)
-      @test buf ≈ baseline
+          method!(buf, @view(lhs[:, k]), rhs[k, :], 1, 0)
+          @test buf ≈ baseline; @test !any(isnan, buf)
+          method!(buf, @view(lhs[:, k]), rhs[k, :], -1, 0)
+          method!(buf, @view(lhs[:, k]), rhs[k, :], 1, 0)
+          @test buf ≈ baseline; @test !any(isnan, buf)
 
-      baseline .+= 2.5 * lhs[:, (k+1):(k+1)] * Matrix(rhs)[(k+1):(k+1), :];
+          baseline .+= 2.5 * lhs[:, (k+1):(k+1)] * rhs[(k+1):(k+1), :];
 
-      fastdensesparsemul_outer!(buf, lhs[:, k+1], rhs[k+1, :], 2.5, 1)
-      @test buf ≈ baseline
-      fastdensesparsemul_outer!(buf, lhs[:, k+1], rhs[k+1, :], -2.5, 1)
-      fastdensesparsemul_outer_threaded!(buf, lhs[:, k+1], rhs[k+1, :], 2.5, 1)
-      @test buf ≈ baseline
+          method!(buf, lhs[:, k+1], rhs[k+1, :], 2.5, 1)
+          @test buf ≈ baseline; @test !any(isnan, buf)
+          method!(buf, lhs[:, k+1], rhs[k+1, :], -2.5, 1)
+          method!(buf, lhs[:, k+1], rhs[k+1, :], 2.5, 1)
+          @test buf ≈ baseline; @test !any(isnan, buf)
+        end
+      end
     end
   end
 end
