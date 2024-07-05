40661fab800f7d62ac77313d2a712621b4d38464
Romeo Valentin <romeov@stanford.edu>
2024-07-05 05:53:20 -0700

    Fixup `mul!` dispatch.

--- src/override.jl
+++ src/override.jl
@@ -5,14 +5,14 @@ function override_mul!(threaded = true)
         @eval function  mul!(C::MatOrView{T}, A::MatOrView{T}, B::SparseMatrixCSC{T}, α::Number, β::Number) where T
             fastdensesparsemul_threaded!(C, A, B, α, β)
         end
-        @eval function mul!(C::MatOrView{T}, a::VecOrView{T}, b::Adjoint{<:SparseVector{T}}, α::Number, β::Number) where T
+        @eval function mul!(C::MatOrView{T}, a::VecOrView{T}, b::Adjoint{T, <:SparseVector{T}}, α::Number, β::Number) where T
             fastdensesparsemul_outer_threaded!(C, a, b', α, β)
         end
     else
         @eval function  mul!(C::MatOrView{T}, A::MatOrView{T}, B::SparseMatrixCSC{T}, α::Number, β::Number) where T
             fastdensesparsemul!(C, A, B, α, β)
         end
-        @eval function mul!(C::MatOrView{T}, a::VecOrView{T}, b::Adjoint{<:SparseVector{T}}, α::Number, β::Number) where T
+        @eval function mul!(C::MatOrView{T}, a::VecOrView{T}, b::Adjoint{T, <:SparseVector{T}}, α::Number, β::Number) where T
             fastdensesparsemul_outer!(C, a, b', α, β)
         end
     end
