68706d78069412e32a59c57c049153653fc90d75
Romeo Valentin <romeov@stanford.edu>
2024-07-05 06:10:17 -0700

    Directly override `(*)` for outer product.

--- src/override.jl
+++ src/override.jl
@@ -1,19 +1,24 @@
 import LinearAlgebra: mul!, Adjoint
+import Base: (*)
 
 function override_mul!(threaded = true)
     if threaded
         @eval function  mul!(C::MatOrView{T}, A::MatOrView{T}, B::SparseMatrixCSC{T}, α::Number, β::Number) where T
             fastdensesparsemul_threaded!(C, A, B, α, β)
         end
-        @eval function mul!(C::MatOrView{T}, a::VecOrView{T}, b::Adjoint{T, <:SparseVector{T}}, α::Number, β::Number) where T
-            fastdensesparsemul_outer_threaded!(C, a, b', α, β)
+        @eval function (*)(a::VecOrView{T}, b::Adjoint{T, <:SparseVector{T}}) where T
+            res = similar(a, length(a), length(b))
+            fastdensesparsemul_outer_threaded!(res, a, b', true, false)
+            res
         end
     else
         @eval function  mul!(C::MatOrView{T}, A::MatOrView{T}, B::SparseMatrixCSC{T}, α::Number, β::Number) where T
             fastdensesparsemul!(C, A, B, α, β)
         end
-        @eval function mul!(C::MatOrView{T}, a::VecOrView{T}, b::Adjoint{T, <:SparseVector{T}}, α::Number, β::Number) where T
-            fastdensesparsemul_outer!(C, a, b', α, β)
+        @eval function (*)(a::VecOrView{T}, b::Adjoint{T, <:SparseVector{T}}) where T
+            res = similar(a, length(a), length(b))
+            fastdensesparsemul_outer!(res, a, b', true, false)
+            res
         end
     end
 end
